aac28d482dea249922bf4046f7c37d457df31407
thanyakorn phanthamit <thanyakornphanthamit@thanyakorns-MacBook-Pro.local>
2020-03-19 18:02:04 +0700

    fix bug eslint

--- functions/index.js
+++ functions/index.js
@@ -9,61 +9,87 @@ const UUID = require('uuid-v4')
 const moment = require('moment')
 const request = require('request-promise')
 const { db, bucket } = require('./config')
-const port = process.env.PORT || 5000
+// const port = process.env.PORT || 5000
 
 const config = {
   channelAccessToken: 'nCmRcgR2JrELs12rskiWB3X4feG6fEwe5OvZkIDICgHQzu+pM9I4yG9iVpbzW2qLerhHKg5931Lvq7hy8ZHR8rr+47omQsUZVp/zDCwJ20ZYxgE0aJMN3wBDaZw8Jyx8Ocsf0ZWYITbC7s8Mr9Y9aQdB04t89/1O/w1cDnyilFU=',
   channelSecret: 'eac5b8a6040ad91451ee24fbc261d7d9'
 };
 var answerArry = []
-var append = false
+var appendBool = false
+var jobIdBool = false
+var jobIdGlobal = ''
 statusSubmit = ''
 const client = new line.Client(config);
+
+app.get('/', (req, res) => {
+  res.send('status code 200 ok')
+})
 
 app.post('/webhook', line.middleware(config), async (req, res) => {
   for (let index = 0; index < req.body.events.length; index++) {
     const event = req.body.events[index];
     const { message, replyToken } = event
-    var clearTime = setTimeout(() => {
-      autoSubmit(answerArry, replyToken)
-    }, 20000) //ทำพร้อมกับที่ส่งข้อความเข้ามา
     console.log(event)
-
+    // var clearTime = setTimeout(() => {
+    //   autoSubmit(answerArry, replyToken)
+    // }, 10000) //ทำพร้อมกับที่ส่งข้อความเข้ามา
     switch (message.text) {
-      case 'submit':
+      case 'submit': {
         console.log('main- 1000')
+        /* eslint-disable no-await-in-loop */
         let data = await processConvertMessages(answerArry)
         console.log('main- 2000')
 
+        /* eslint-disable no-await-in-loop */
         let refJob = await saveToFirestore(data)
         console.log('main- 3000')
 
         replyRefJob(replyToken, refJob)
         console.log('main- 3100')
 
+        /* eslint-disable no-await-in-loop */
         let user = await fetchCurrentEmployee()
         console.log('main- 4000')
 
-        await pushLineMessageApi(user.uidLine)
+        pushLineMessageApi(user.uidLine)
         console.log('main- 5000')
         answerArry = []
         statusSubmit = 'submit'
-        clearTimeout(clearTime) // clear setTimeOut เมื่อกด submit
         break;
+      }
       case 'append':
         client.replyMessage(replyToken, { type: 'text', text: 'ขอทราบรหัสงาน' })
-        append = true
+        appendBool = true
         break;
-      case 'working':
+      case 'working': {
+        /* eslint-disable no-await-in-loop */
         let currentUser = await fetchCurrentEmployee()
         client.replyMessage(replyToken, { type: 'text', text: currentUser.displayName})
         break;
+      }
       default:
-        if (append) {
-          appendJob(message, replyToken) 
+        if (appendBool) {
+          if (!jobIdBool) {
+            searchJobsId(message, replyToken)
+            console.log('search')
+          } else {
+            switch (message.text) {
+              case 'submit up': {
+                appendJob(answerArry, jobIdGlobal, replyToken)
+                console.log('append')
+                break;
+              }
+              default:
+                answerArry.push(message)
+                console.log('push')
+                console.log(answerArry)
+              break;
+            }
+          }
         } else {
-          answerArry.push(message)
           statusSubmit = 'pending'
+          answerArry.push(message)
         }
         break;
     }
@@ -71,26 +97,27 @@ app.post('/webhook', line.middleware(config), async (req, res) => {
   res.send("OK")
 })
 
-
 // SUBMIT
 async function processConvertMessages(answerArry) {
   let messages = []
   for (let index = 0; index < answerArry.length; index++) {
     const message = answerArry[index];
     if (message.type === 'text') {
-      var msg = {
+      let msg = {
         type: message.type,
         content: message.text
       }
-    } else if (message.type === 'image') {
+      messages.push(msg)
+    } else {
       let bufferArr = await decodeBase64(message.id)
-      let url = await uploadStorage(message.id, bufferArr)
-      var msg = {
+      let _file = fileExtension(message)
+      let url = await uploadStorage(message.id, bufferArr, _file)
+      let msg = {
         type: message.type,
         content: url
       }
+      messages.push(msg)
     }
-    messages.push(msg)
   }
   return messages
 }
@@ -110,29 +137,38 @@ async function decodeBase64 (messageID) {
     return bufferArr
 }
 
-async function uploadStorage(messagID, bufferArr) {
-  try {
-    for (let i = 0; i < bufferArr.length; i++) {
-      const buffer = bufferArr[i];
-      const tempLocalFile = path.join(os.tmpdir(), 'photo.jpg') // create directory ชั่วคราว พร้อมกับตั้งชื่อไฟล์
-      fs.writeFileSync(tempLocalFile, buffer)  // เขียนไฟล์โดนใช้ ชื่อ directory
-      console.log('300')
-      await bucket.upload(tempLocalFile, {
-        destination: `${messagID}.jpg`,
-        metadata: {
-          cacheControl: 'no-cache',
-          metadata: {
-            firebaseStorageDownloadTokens: UUID()
-          }
-        }
-      })
-      fs.unlinkSync(tempLocalFile)
-      console.log('301')
-      return `https://firebasestorage.googleapis.com/v0/b/chatbot-it-support.appspot.com/o/${messagID}.jpg?alt=media`
-    }
-  } catch (err) {
-    console.log(err)
+function fileExtension(message) {
+  console.log('202')
+  var _file = ''
+  if (message.type === 'video') {
+    _file = 'mp4'
+  } else if (message.type === 'image') {
+    _file = 'jpg'
+  } else if (message.type === 'audio') {
+    _file = 'acc'
   }
+  return _file
+}
+
+async function uploadStorage(messageID, bufferArr, _file) {
+  for (let i = 0; i < bufferArr.length; i++) {
+    const buffer = bufferArr[i];
+    const tempLocalFile = path.join(os.tmpdir(), `fileName.${_file}`) // create directory ชั่วคราว พร้อมกับตั้งชื่อไฟล์
+    fs.writeFileSync(tempLocalFile, buffer)  // เขียนไฟล์โดยใช้ ชื่อ directory คือ tempLocalFile
+    console.log('300')
+    await bucket.upload(tempLocalFile, {
+      destination: `${messageID}.${_file}`,
+      metadata: {
+        cacheControl: 'no-cache',
+        metadata: {
+          firebaseStorageDownloadTokens: UUID()
+        }
+      }
+    })
+    fs.unlinkSync(tempLocalFile)
+    console.log('301')
+  }
+  return `https://firebasestorage.googleapis.com/v0/b/chatbot-it-support.appspot.com/o/${messageID}.${_file}?alt=media`
 }
 
 async function fetchCurrentEmployee() {
@@ -140,20 +176,21 @@ async function fetchCurrentEmployee() {
   return currentEmployee.data()
 }
 
-async function saveToFirestore(answer, message) {
-  let randomNumber = (Math.random() * 1000000).toFixed(0)
+async function saveToFirestore(answer) {
+  let randomNumber = (Math.random() * 1000).toFixed(0)
+  let jobId = `${moment().format('YYYYMMDD')}#${randomNumber}`
   let data = {
     createdTimestamp: moment().format('x'),
     message: answer,
     status: '',
-    ref: randomNumber
+    jobId: jobId
   }
   db.collection('jobs').add(data)
-  return randomNumber
+  return jobId
 }
 
 function replyRefJob(replyToken, refJob) {
-  let msg = {
+  var msg = {
     type: 'text',
     text: `รหัสงาน: ${refJob}`
   }
@@ -173,65 +210,79 @@ async function pushLineMessageApi(lineUid) {
       stickerResourceType: 'STATIC'
     }
   ]
+  answerArry = []
   client.pushMessage(lineUid, msg)
 }
 
-function autoReplyMessage(replyToken) {
-  let msg = {
-    type: 'text',
-    text: 'Submit Success!!'
-  }
-  client.replyMessage(replyToken, msg)
-}
-
 async function autoSubmit (answerArry, replyToken) {
-  try {
-
-    if (statusSubmit === 'pending') { 
-      statusSubmit = 'submit'
-      let data = await processConvertMessages(answerArry)
-      let refJob = await saveToFirestore(data)
-      replyRefJob(replyToken, refJob)
-      let user = await fetchCurrentEmployee()
-      await pushLineMessageApi(user.uidLine)
-      autoSubmit(answerArry)
-      autoReplyMessage(replyToken)
-      console.log('submit success')
-    } else {
-      console.log('worker')
-    }
-  } catch (e) {
-    console.error(e)
+  if (statusSubmit === 'pending') { 
+    statusSubmit = 'submit'
+    let data = await processConvertMessages(answerArry)
+    let refJob = await saveToFirestore(data)
+    replyRefJob(replyToken, refJob)
+    let user = await fetchCurrentEmployee()
+    await pushLineMessageApi(user.uidLine)
+    // autoReplyMessage(replyToken)
+    answerArry = []
+    console.log('submit success')
+  } else {
+    console.log('worker')
   }
 }
+
+// function autoReplyMessage(replyToken) {
+//   let msg = {
+//     type: 'text',
+//     text: 'Submit Success!!'
+//   }
+//   client.replyMessage(replyToken, msg)
+// }
 // SUBMIT
 
 
 
 // APPEND
-async function appendJob(message, replyToken) {
-  let jobDocId = await searchRefJobs(message)
-  if (jobDocId) {
-    var job = await db.collection('jobs').doc(jobDocId).get()
-    job = job.data()
+// todo: ทำให้ไม่เขียนทับ
+async function appendJob(answerArry, jobId, replyToken) {
+  let data = await processConvertMessages(answerArry)
+  db.collection('jobs').doc(jobId).update({
+    append: data
+  })
+  updateSuccess(replyToken)
+  answerArry = []
+  jobIdBool = false
+  appendBool = false
+}
+
+function updateSuccess(replyToken) {
+  client.replyMessage(replyToken, { type: 'text', text: 'อัพเดทสำเร็จ' })
+}
+
+async function searchJobsId(message, replyToken) {
+  var jobId = null
+  let refJob = await db.collection('jobs').where('jobId', '==' , message.text).get()
+  refJob.forEach(snapshot => {
+    jobId = snapshot.id
+  })
+  replyMessageJob(replyToken, jobId)
+  jobIdGlobal = jobId
+}
+
+async function replyMessageJob(replyToken, jobId) {
+  if (jobId) {
+    // var job = await db.collection('jobs').doc(jobId).get()
+    // job = job.data()
+    client.replyMessage(replyToken, { type: 'text', text: 'เจองานแย้ว'})
+    jobIdBool = true
   } else {
     client.replyMessage(replyToken, { type: 'text', text: 'ไม่พบงาน' })
-    append = false
+    appendBool = false
   }
-  console.log(job)
 }
+// APPEND
 
-async function searchRefJobs(message) {
-  var jobDocId = ''
-  let refJob = await db.collection('jobs').where('ref', '==' , message.text).get()
-  refJob.forEach(snapshot => {
-    jobDocId = snapshot.id
-  })
-  return jobDocId
-}
-
-app.listen(port, () => {
-  console.log(`listening on ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`listening on ${port}`)
+// })
 
 exports.messagingAPI = functions.https.onRequest(app)
